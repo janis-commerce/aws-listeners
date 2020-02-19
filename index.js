894084586a1f90cf4dd0187d997baed2e89d4ddf
manuelvilche <manuelvilche@gmail.com>
2020-02-19 17:39:38 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { AwsListeners } = require('./lib');
+
+module.exports = AwsListeners;
---
+++ tests/aws-listeners-test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const assert = require('assert');
+
+const sandbox = require('sinon').createSandbox();
+
+const AwsListeners = require('./../index');
+
+const AwsListenersError = require('./../lib/aws-listeners-error')
+
+describe('AwsListeners', () => {
+
+	// your tests here...
+
+});
